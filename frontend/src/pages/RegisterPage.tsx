1405fd4b803c7ca4728da55b34fe9ae178f4aaa2
zlqkhokhar1-creator <zlqkhokhar1@gmail.com>
2025-10-17 23:22:56 +0800

    fix 7

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -190,7 +190,7 @@ const RegisterPage: React.FC = () => {
                 <Checkbox
                   id="terms"
                   checked={agreeToTerms}
-                    onCheckedChange={(checked) => setAgreeToTerms(checked === true)}
+                  onCheckedChange={(checked) => setAgreeToTerms(checked as boolean)}
                 />
                 <Label htmlFor="terms" className="text-sm">
                   I agree to the{' '}
